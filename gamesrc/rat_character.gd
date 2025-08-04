9ba429ed60bc7eccae394c04d6118a3f218032dd
Justin <peterjust.d@gmail.com>
2025-08-03 20:52:47 -0500

    I bwoke dusk

--- gamesrc/rat_character.gd
+++ gamesrc/rat_character.gd
@@ -1,17 +1,16 @@
 extends CharacterBody2D
 @onready var sprite_2d = $Sprite2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var ladder_ray_cast: RayCast2D = $LadderRayCast2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var jump_charge = 0
 
-
 func _physics_process(delta: float) -> void:
 	var ladderCollider = ladder_ray_cast.get_collider()
 	if ladderCollider: _ladder_climb(delta)
 	else:  _movement(delta)
-		
 	
 	#set_animation()
 
@@ -23,15 +22,13 @@ func _ladder_climb(delta):
 	var direction := Vector2.ZERO
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-	sprite_2d.animation = "climb"
+	animated_sprite_2d.animation = "climb"
 	if direction: velocity = direction * SPEED /2
 	
 	else: velocity = Vector2.ZERO
-	
-	
 
 func _movement(delta):
-	sprite_2d.animation = "walk"
+	animated_sprite_2d.animation = "walk"
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -46,10 +43,6 @@ func _movement(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	
-	
-		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -64,12 +57,3 @@ func _movement(delta):
 	
 		
 	#if Input.is_action_just_released()
-	
-
-
-
-	
-
-
-
-	
